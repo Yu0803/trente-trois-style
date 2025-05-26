3d45b5bf3e32b96b32ac0e8f1f531b1ccc811e4d
Yu Tmym <06eb091l@gmail.com>
2025-05-26 21:59:48 +0900

    fixed footer layout フッターのレイアウトを修正しました

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,29 +1,30 @@
 <!-- Benefit Section -->
+
 <section class="benefit-section container-wide">
   <div class="benefit-box">
-    
-    <div><i class="fa-solid fa-truck-fast"></i>
+    <div>
+      <i class="fa-solid fa-truck-fast"></i>
       <strong>FREE SHIPPING</strong>
       <p>Enjoy free shipping on all orders above $100</p>
     </div>
   </div>
   <div class="benefit-box">
-    
-    <div><i class="fas fa-life-ring"></i>
+    <div>
+      <i class="fas fa-life-ring"></i>
       <strong>SUPPORT 24/7</strong>
       <p>Our support team is there to help you for queries</p>
     </div>
   </div>
   <div class="benefit-box">
-    
-    <div><i class="fas fa-rotate-left"></i>
+    <div>
+      <i class="fas fa-rotate-left"></i>
       <strong>30 DAYS RETURN</strong>
       <p>Simply return it within 30 days for an exchange.</p>
     </div>
   </div>
   <div class="benefit-box">
-    
-    <div><i class="fa-solid fa-fingerprint"></i>
+    <div>
+      <i class="fa-solid fa-fingerprint"></i>
       <strong>100% PAYMENT SECURE</strong>
       <p>Our payments are secured with 256 bit encryption</p>
     </div>
@@ -32,63 +33,64 @@
 
 <!-- Footer -->
 <footer class="footer">
-  <div class="footer-container container-wide">
-    <div class="footer-column">
-      <h4>COMPANY INFO</h4>
-      <ul>
-        <li><a href="#">About Us</a></li>
-        <li><a href="#">Latest Posts</a></li>
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">Contact Us</a></li>
-        <li><a href="#">Shop</a></li>
-        <li><a href="#">Store Location</a></li>
-      </ul>
+  <!-- 👇 container-wide はここ1回だけ -->
+  <div class="container-wide">
+    <div class="footer-container">
+      <div class="footer-column">
+        <h4>COMPANY INFO</h4>
+        <ul>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Latest Posts</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Contact Us</a></li>
+          <li><a href="#">Shop</a></li>
+          <li><a href="#">Store Location</a></li>
+        </ul>
+      </div>
+
+      <div class="footer-column">
+        <h4>HELP LINKS</h4>
+        <ul>
+          <li><a href="#">Returns</a></li>
+          <li><a href="#">Order Status</a></li>
+          <li><a href="#">Size Guide</a></li>
+          <li><a href="#">Delivery</a></li>
+          <li><a href="#">Shipping Info</a></li>
+          <li><a href="#">FAQ</a></li>
+        </ul>
+      </div>
+
+      <div class="footer-column">
+        <h4>USEFUL LINKS</h4>
+        <ul>
+          <li><a href="#">Special Offers</a></li>
+          <li><a href="#">Gift Cards</a></li>
+          <li><a href="#">Advertising</a></li>
+          <li><a href="#">Terms of Use</a></li>
+        </ul>
+      </div>
+
+      <div class="footer-column newsletter">
+        <h4>GET IN THE KNOW</h4>
+        <form>
+          <input type="email" placeholder="Enter email">
+          <button type="submit">→</button>
+        </form>
+      </div>
     </div>
 
-    <div class="footer-column">
-      <h4>HELP LINKS</h4>
-      <ul>
-        <li><a href="#">Returns</a></li>
-        <li><a href="#">Order Status</a></li>
-        <li><a href="#">Size Guide</a></li>
-        <li><a href="#">Delivery</a></li>
-        <li><a href="#">Shipping Info</a></li>
-        <li><a href="#">FAQ</a></li>
-      </ul>
-    </div>
-
-    <div class="footer-column">
-      <h4>USEFUL LINKS</h4>
-      <ul>
-        <li><a href="#">Special Offers</a></li>
-        <li><a href="#">Gift Cards</a></li>
-        <li><a href="#">Advertising</a></li>
-        <li><a href="#">Terms of Use</a></li>
-      </ul>
-    </div>
-
-    <div class="footer-column newsletter">
-      <h4>GET IN THE KNOW</h4>
-      <form>
-        <input type="email" placeholder="Enter email">
-        <button type="submit">→</button>
-      </form>
-    </div>
-  </div>
-
-  
-
-  <div class="footer-bottom container-wide">
-    <div class="footer-info">
-      <p>© 2020 eCommerce, Made by DeoThemes.</p>
-      <a href="#">Privacy Policy</a>
-      <a href="#">Terms & Conditions</a>
-    </div>
-    <div class="footer-icons">
-      <img src="{{ asset('images/visa.png') }}" alt="Visa">
-      <img src="{{ asset('images/master.png') }}" alt="Mastercard">
-      <img src="{{ asset('images/paypal.png') }}" alt="PayPal">
-      <img src="{{ asset('images/visaelectron.png') }}" alt="Visa Electron">
+    <div class="footer-bottom">
+      <div class="footer-info">
+        <p>© 2020 eCommerce, Made by DeoThemes.</p>
+        <a href="#">Privacy Policy</a>
+        <a href="#">Terms & Conditions</a>
+      </div>
+      <div class="footer-icons">
+        <img src="{{ asset('images/visa.png') }}" alt="Visa">
+        <img src="{{ asset('images/master.png') }}" alt="Mastercard">
+        <img src="{{ asset('images/paypal.png') }}" alt="PayPal">
+        <img src="{{ asset('images/visaelectron.png') }}" alt="Visa Electron">
+      </div>
     </div>
   </div>
 </footer>
