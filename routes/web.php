f3fb25bddc6a121c1bdc6237888a2fe33dd66829
Yu Tmym <06eb091l@gmail.com>
2025-06-05 19:41:02 +0900

    created admin ligin page

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -96,3 +98,17 @@ Route::get('/', [TopPageController::class, 'index']);
 Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 // フォーム送信処理
 Route::post('/contact', [ContactController::class, 'submit'])->name('contact.submit');
+
+
+
+// =======================
+// Adminトップページ
+// =======================
+Route::get('/admin/login', [AuthController::class, 'showLoginForm'])->name('admin.login');
+Route::post('/admin/login', [AuthController::class, 'login'])->name('admin.login.submit');
+Route::post('/admin/logout', [AuthController::class, 'logout'])->name('admin.logout');
+
+Route::middleware('auth:admin')->group(function () {
+    Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+});
+
