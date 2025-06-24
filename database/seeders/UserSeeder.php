f63852de43244997ba480e8e40b260971ba1bce6
Yu Tmym <06eb091l@gmail.com>
2025-06-24 21:17:37 +0900

    created backend of order management

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,26 +10,53 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-
-
         $faker = Faker::create();
         $countries = ['Japan', 'France', 'USA', 'Germany', 'Brazil', 'China', 'India', 'Italy', 'Egypt', 'Canada'];
 
+        // ランダムなユーザー10人
         foreach (range(1, 10) as $index) {
             User::create([
                 'first_name' => $faker->firstName,
                 'last_name' => $faker->lastName,
                 'email' => $faker->unique()->safeEmail,
                 'phone' => $faker->phoneNumber,
-                //'birth_of_date' => $faker->date(),
-                'dob' => $faker->date(), // ← 両方必要なら
+                'dob' => $faker->date(),
                 'address1' => $faker->streetAddress,
                 'address2' => $faker->secondaryAddress,
                 'postal_code' => $faker->postcode,
                 'country' => $countries[array_rand($countries)],
-                //'status' => 'active',
                 'password' => bcrypt('password'),
             ]);
         }
+
+        // 固定ユーザー（Emily）
+        User::create([
+            'first_name' => 'Emily',
+            'last_name' => 'Smith',
+            'email' => 'emily@example.com',
+            'phone' => '080-1111-1111',
+            'dob' => '1990-01-01',
+            'address1' => 'Tokyo',
+            'address2' => '',
+            'postal_code' => '1000001',
+            'country' => 'Japan',
+            'password' => bcrypt('password'),
+        ]);
+
+        // Sarah
+        User::create([
+            'first_name' => 'Sarah',
+            'last_name' => 'Johnson',
+            'email' => 'sarah@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        // Michael
+        User::create([
+            'first_name' => 'Michael',
+            'last_name' => 'Brown',
+            'email' => 'michael@example.com',
+            'password' => bcrypt('password'),
+        ]);
     }
 }
